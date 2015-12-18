475804143fe53a310b814f52022f8b87694b73dc
Mikhail Dudin <dudin-m@live.ru>
2015-12-18 23:57:03 +0300

    Answer bound to card's state

--- client/scripts/react_compoments/Card.js
+++ client/scripts/react_compoments/Card.js
@@ -1,13 +1,23 @@
 var React = require('react');
 
 module.exports = React.createClass({
+    checkAnswer: function(currentCard) {
+        if (currentCard.eng == currentCard.rus) {
+            alert('correct');
+        }
+    },
+
+    handleAnswerChange: function(event) {
+        this.setState({ answer: event.target.value });
+    },
+
     render: function() {
         var currentCard = this.props.cardToDisplay;
 
         return (
             <div className="card">
                 <span>{currentCard.eng}</span><br/>
-                <input type="test" />
+                <input type="text" onChange={this.handleAnswerChange} />
                 <button>Ответить</button>
             </div>
         );
